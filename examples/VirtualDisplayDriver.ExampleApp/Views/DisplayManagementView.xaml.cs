3ad68c2897b10d44050e3387bae23232441057eb
Morten Brudvik <morten@brudvik.com>
2026-03-15 09:02:23 +0100

    feat: Add link to open Windows display settings in `DisplayManagementView`
    - Added a hyperlink to `DisplayManagementView` allowing users to directly open Windows display settings.
    - Implemented click event to launch the "ms-settings:display" URI using `Process.Start`.

--- examples/VirtualDisplayDriver.ExampleApp/Views/DisplayManagementView.xaml.cs
+++ examples/VirtualDisplayDriver.ExampleApp/Views/DisplayManagementView.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace VirtualDisplayDriver.ExampleApp.Views;
@@ -7,5 +9,10 @@ public partial class DisplayManagementView : UserControl
     public DisplayManagementView()
     {
         InitializeComponent();
+    }
+
+    private void OpenWindowsDisplaySettings_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo("ms-settings:display") { UseShellExecute = true });
     }
 }
